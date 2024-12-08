19df19c7b186d35fff15841eda60f813fda157e5
erofimova <e.erofimova@gmail.com>
2024-12-08 23:23:19 +0200

    Vehicle

--- TestApp.UnitTests/VehicleTests.cs
+++ TestApp.UnitTests/VehicleTests.cs
@@ -8,25 +8,44 @@ namespace TestApp.UnitTests;
 
 public class VehicleTests
 {
-    // TODO: write the setup method
+  
+    Vehicles _vehicles;
 
-    // TODO: finish test
+    [SetUp]
+    public void SetUp()
+    {
+        _vehicles = new Vehicles();
+    }
     [Test]
     public void Test_AddAndGetCatalogue_ReturnsSortedCatalogue()
     {
         // Arrange
-        string expected = $"Cars:{Environment.NewLine}Ford: Focus - 120hp{Environment.NewLine}Toyota: Camry - 150hp{Environment.NewLine}Trucks:{Environment.NewLine}Volvo: VNL - 500kg";
+        string[] input = new string[] { "Car/Toyota/Camry/150", "Truck/Volvo/VNL/500", "Car/Ford/Focus/120" };
+
+        string expected = $"Cars:{Environment.NewLine}" +
+                          $"Ford: Focus - 120hp{Environment.NewLine}" +
+                          $"Toyota: Camry - 150hp{Environment.NewLine}" +
+                          $"Trucks:{Environment.NewLine}" +
+                          $"Volvo: VNL - 500kg";
 
         // Act
-        //string result = this._vehicles.AddAndGetCatalogue(input);
+        string result = this._vehicles.AddAndGetCatalogue(input);
 
         // Assert
-        //Assert.That(result, Is.EqualTo(expected));
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_AddAndGetCatalogue_ReturnsEmptyCatalogue_WhenNoDataGiven()
     {
-        // TODO: finish test
+        string[] vehicles = Array.Empty<string>();
+        string expected = $"Cars:{Environment.NewLine}" +
+                          $"Trucks:";
+
+        // Act
+        string result = this._vehicles.AddAndGetCatalogue(vehicles);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
