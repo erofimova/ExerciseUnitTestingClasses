a46e6e52ebef72229110490632e34e741cf1ee01
erofimova <e.erofimova@gmail.com>
2024-12-08 22:59:09 +0200

    Song

--- TestApp.UnitTests/SongTests.cs
+++ TestApp.UnitTests/SongTests.cs
@@ -14,7 +14,7 @@ public class SongTests
         this._song = new();
     }
 
-    // TODO: finish test
+
     [Test]
     public void Test_AddAndListSongs_ReturnsAllSongs_WhenWantedListIsAll()
     {
@@ -23,20 +23,37 @@ public class SongTests
         string expected = $"Song1{Environment.NewLine}Song2{Environment.NewLine}Song3";
 
         // Act
+        string result = _song.AddAndListSongs(songs, "all");
 
         // Assert
-        //Assert.That(result, Is.EqualTo(expected));
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_AddAndListSongs_ReturnsFilteredSongs_WhenWantedListIsSpecific()
     {
-        // TODO: finish test
+        // Arrange
+        string[] songs = { "Pop_Song1_3:30", "Rock_Song2_4:15", "Pop_Song3_3:00" };
+        string expected = "Song2";
+
+        // Act
+        string result = _song.AddAndListSongs(songs, "Rock");
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_AddAndListSongs_ReturnsEmptyString_WhenNoSongsMatchWantedList()
     {
-        // TODO: finish test
+        // Arrange
+        string[] songs = { "Pop_Song1_3:30", "Pop_Song3_3:00" };
+        string wantedList = "Rock";
+
+        // Act
+        string result = _song.AddAndListSongs(songs, wantedList);
+
+        // Assert
+        Assert.That(result, Is.Empty);
     }
 }
