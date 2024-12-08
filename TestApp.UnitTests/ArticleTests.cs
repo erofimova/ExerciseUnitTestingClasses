18816abb48aa42257c3012aa013df4f9e5c2bd82
erofimova <e.erofimova@gmail.com>
2024-12-08 23:36:57 +0200

    Article

--- TestApp.UnitTests/ArticleTests.cs
+++ TestApp.UnitTests/ArticleTests.cs
@@ -6,32 +6,63 @@ namespace TestApp.UnitTests;
 
 public class ArticleTests
 {
-    // TODO: write the setup method
+    private Article _article;
 
-    // TODO: finish test
+    [SetUp]
+    public void SetUp()
+    {
+        _article = new Article();
+    }
+
     [Test]
     public void Test_AddArticles_ReturnsArticleWithCorrectData()
     {
         // Arrange
+        string[] articles = new string[] { "Article#1 Content1 Author1", "Article#2 Content2 Author2", "Article#3 Content3 Author3" };
+        string firstArticleTitle = "Article#1";
+        string secondArticleContent = "Content2";
+        string thirdArticleAuthor = "Author3";
 
         // Act
+        Article result = _article.AddArticles(articles);
 
         // Assert
-        //Assert.That(result.ArticleList, Has.Count.EqualTo(3));
-        //Assert.That(result.ArticleList[0].Title, Is.EqualTo("Article"));
-        //Assert.That(result.ArticleList[1].Content, Is.EqualTo("Content2"));
-        //Assert.That(result.ArticleList[2].Author, Is.EqualTo("Author3"));
+        Assert.That(result.ArticleList, Has.Count.EqualTo(3));
+        Assert.That(result.ArticleList[0].Title, Is.EqualTo(firstArticleTitle));
+        Assert.That(result.ArticleList[1].Content, Is.EqualTo(secondArticleContent));
+        Assert.That(result.ArticleList[2].Author, Is.EqualTo(thirdArticleAuthor));
     }
 
     [Test]
     public void Test_GetArticleList_SortsArticlesByTitle()
     {
-        // TODO: finish test
+        // Arrange
+        string[] articles = new string[] { "SeaGarden Content2 Author2", "NewPresident Content1 Author1", "RadioNews Content3 Author3" };
+
+        Article testArticle = _article.AddArticles(articles);
+        string expected = $"NewPresident - Content1: Author1{Environment.NewLine}" +
+                          $"RadioNews - Content3: Author3{Environment.NewLine}" +
+                          $"SeaGarden - Content2: Author2";
+
+        // Act
+        string result = _article.GetArticleList(testArticle, "title");
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_GetArticleList_ReturnsEmptyString_WhenInvalidPrintCriteria()
     {
-        // TODO: finish test
+        // Arrange
+        string[] articles = new string[] { "SeaGarden Content2 Author2", "NewPresident Content1 Author1", "RadioNews Content3 Author3" };
+
+        Article testArticle = _article.AddArticles(articles);
+
+        // Act
+        string result = _article.GetArticleList(testArticle, "invalid");
+
+        // Assert
+        Assert.That(result, Is.Empty);
     }
 }
